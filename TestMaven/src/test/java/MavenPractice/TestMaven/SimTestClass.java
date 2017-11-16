95b2f5ab3a4ceab2bcce3761dcdaefc76f7d6922
Indu <Indu@NandusLaptop>
2017-11-16 17:20:44 -0500

    dfgsdfgsfgsdfg

--- TestMaven/src/test/java/MavenPractice/TestMaven/SimTestClass.java
+++ TestMaven/src/test/java/MavenPractice/TestMaven/SimTestClass.java
@@ -1,10 +1,19 @@
 package MavenPractice.TestMaven;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class SimTestClass {
 	@Test
 	public void logintest()
+	{
+		System.out.println("first");
+		
+	}
+	WebDriver driver=new ChromeDriver();
+	@Test
+	public void logintest1()
 	{
 		System.out.println("first");
 		
