097059440f2c440050807274f4f86bf9417d118b
Indu <Indu@NandusLaptop>
2017-11-16 17:09:32 -0500

    second commit

---
+++ TestMaven/src/test/java/MavenPractice/TestMaven/SimTestClass.java
@@ -0,0 +1,14 @@
+package MavenPractice.TestMaven;
+
+import org.testng.annotations.Test;
+
+public class SimTestClass {
+	@Test
+	public void logintest()
+	{
+		System.out.println("first");
+		
+	}
+	
+
+}
